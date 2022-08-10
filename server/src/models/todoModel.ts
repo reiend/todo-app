d54bfac31dd7f06090bbf6129fbd24b178c73c94
reiend <loncerasjakerei@gmail.com>
2022-08-10 23:54:18 +0800

    TodoModel validation

--- server/src/models/todoModel.ts
+++ server/src/models/todoModel.ts
@@ -4,8 +4,8 @@ const TodoSchema = new Mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: true,
-    maxLength: 128
+    required: [true, 'name is required'],
+    maxLength: [128, "name can't be more than 128 characters"]
   },
   completed: {
     type: Boolean,
