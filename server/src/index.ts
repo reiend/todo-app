bc153e5740ce9fbc26ad47a1d852d59a9957917f
reiend <loncerasjakerei@gmail.com>
2022-08-10 22:04:48 +0800

    middleware for req body

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// allow to read and write on request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
 app.use('/api/v1', router.todoRouter);
 
+// run server
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
