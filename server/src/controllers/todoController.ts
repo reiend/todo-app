df642fae075196d8f87e9f0b5abfc24fb707756d
reiend <loncerasjakerei@gmail.com>
2022-08-11 00:45:21 +0800

    error handling controllers

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -12,9 +12,13 @@ interface TodoProps {
 
 const Todo: TodoProps = {
   getTodos: async (req, res) => {
-    const todos = await TodoModel.find();
+    try {
+      const todos = await TodoModel.find();
 
-    res.status(200).json({ todos });
+      res.status(200).json({ todos });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   },
 
   createTodo: async (req, res) => {
@@ -22,15 +26,29 @@ const Todo: TodoProps = {
       res.status(400);
       throw new Error('please add name field');
     }
+    try {
+      const newTodo = await TodoModel.create(req.body);
 
-    const newTodo = await TodoModel.create(req.body);
-
-    res.status(201).json({ todo: newTodo });
+      res.status(201).json({ todo: newTodo });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   },
 
   getTodo: async (req, res) => {
-    const foundTodo = await TodoModel.findOne({ _id: req.params.id });
-    res.status(200).json({ todo: foundTodo });
+    try {
+      const foundTodo = await TodoModel.findOne({ _id: req.params.id });
+
+      if (!foundTodo) {
+        return res
+          .status(404)
+          .json({ message: `No Todo has an id of ${req.params.id}` });
+      }
+
+      res.status(200).json({ todo: foundTodo });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   },
 
   updateTodo: async (req, res) => {
@@ -39,21 +57,41 @@ const Todo: TodoProps = {
       throw new Error('please add name and completed field');
     }
 
-    const updatedTodo = await TodoModel.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      { new: true }
-    );
+    try {
+      const updatedTodo = await TodoModel.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true }
+      );
 
-    res.status(200).json({ todo: updatedTodo });
+      if (!updatedTodo) {
+        return res
+          .status(404)
+          .json({ message: `No Todo has an id of ${req.params.id}` });
+      }
+
+      res.status(200).json({ todo: updatedTodo });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   },
 
   deleteTodo: async (req, res) => {
-    const deletedTodo = await TodoModel.findOneAndDelete({
-      _id: req.params.id
-    });
+    try {
+      const deletedTodo = await TodoModel.findOneAndDelete({
+        _id: req.params.id
+      });
 
-    res.status(200).json({ todo: deletedTodo });
+      if (!deletedTodo) {
+        return res
+          .status(404)
+          .json({ message: `No Todo has an id of ${req.params.id}` });
+      }
+
+      res.status(200).json({ todo: deletedTodo });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   }
 };
 
