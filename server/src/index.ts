3dc5bd0525fb1638adbc1b437b125ed926393897
reiend <loncerasjakerei@gmail.com>
2022-08-10 21:27:35 +0800

    added initial routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// routes
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).json({ message: 'list of todos' });
+});
+
+app.post('/api/v1/todo', (req, res) => {
+  res.status(200).json({ message: 'created todo' });
+});
+
+app.put('/api/v1/todo/:id/mark', (req, res) => {
+  res.status(200).json({ message: `mark todo ${req.params.id}` });
+});
+
+app.delete('/api/v1/todo/:id', (req, res) => {
+  res.status(200).json({ message: `deleted todo ${req.params.id}` });
 });
 
 app.listen(port, () => {
