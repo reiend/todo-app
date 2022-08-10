fa2ca57926bd0e22130f50870cb0318ec0f6fdec
reiend <loncerasjakerei@gmail.com>
2022-08-10 22:16:21 +0800

    overriding express default error handler

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -14,10 +14,19 @@ const Todo: TodoProps = {
   },
 
   create: (req, res) => {
+    if (!req.body.name) {
+      res.status(400);
+      throw new Error('please add name field');
+    }
     res.status(200).json({ message: 'created todo' });
   },
 
   status: (req, res) => {
+    if (!req.body.status) {
+      res.status(400);
+      throw new Error('please add status field');
+    }
+    res.status(200).json({ message: 'created todo' });
     res.status(200).json({
       message: `update status todo completed or not ${req.params.id}`
     });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import errorHandler from './middlewares/errorMiddleware';
 
 // allow the use of env file
 dotenv.config();
@@ -14,6 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use('/api/v1', router.todoRouter);
+
+// override default express error handler
+app.use(errorHandler);
 
 // run server
 app.listen(port, () => {
