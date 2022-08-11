b45c2f115c0c9d84602a9fccfd877d6e7379d9a5
reiend <loncerasjakerei@gmail.com>
2022-08-11 08:43:30 +0800

    climbing up states With TodoContainer

---
+++ client/src/components/TodoContainer.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { getTodos } from '../services/todo';
+
+import TodoCreateForm from './TodoCreateForm';
+import TodoList from './TodoList';
+
+const TodoContainer = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await getTodos()
+        .then(res => {
+          setTodos(res.data.todos);
+        })
+        .catch(() => {
+          return <div>error</div>;
+        });
+    }, 1500);
+  }, [todos.length]);
+
+  return (
+    <>
+      <TodoCreateForm setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </>
+  );
+};
+
+export default TodoContainer;
