570f5f76a623ff8eef9d46ba3304ce10250a0846
reiend <loncerasjakerei@gmail.com>
2022-08-11 09:34:28 +0800

    added spinner first time loading todos

--- client/src/components/Todo/TodoContainer.tsx
+++ client/src/components/Todo/TodoContainer.tsx
@@ -3,15 +3,18 @@ import { getTodos } from '../../services/todo';
 
 import TodoCreateForm from './TodoCreateForm';
 import TodoList from './TodoList';
+import Spinner from '../Spinner';
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     setTimeout(async () => {
       await getTodos()
         .then(res => {
           setTodos(res.data.todos);
+          setIsLoaded(true);
         })
         .catch(() => {
           return <div>error</div>;
@@ -22,7 +25,7 @@ const TodoContainer = () => {
   return (
     <>
       <TodoCreateForm setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      {isLoaded ? <TodoList todos={todos} setTodos={setTodos} /> : <Spinner />}
     </>
   );
 };
