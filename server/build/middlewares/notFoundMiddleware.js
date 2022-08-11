9cc651f8b84a190bf340ec37ed0433402ada1a9c
reiend <loncerasjakerei@gmail.com>
2022-08-11 10:17:09 +0800

    ignore server build

--- server/build/middlewares/notFoundMiddleware.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundHandler = (req, res) => res.status(404).json({ message: "Route does't exist" });
-exports.default = notFoundHandler;
-//# sourceMappingURL=notFoundMiddleware.js.map
